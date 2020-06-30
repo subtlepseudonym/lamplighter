f8c4f78a4a5d758527fe34c93797d44760217148
Connor Demille <subtlepseudonym@gmail.com>
2020-06-29 20:54:43 -0400

    Add function for calculating julian date

---
+++ solar/julian.go
@@ -0,0 +1,24 @@
+package solar
+
+import (
+	"time"
+)
+
+const (
+	EpochJulianDate = 2440587.5
+	TerrestrialTimeOffset = 42.184 // TT offset from unix time
+	SecondsPerDay = 86400 // not including leap seconds
+)
+
+// JulianDate returns the Julian date for a particular time,
+// including leap seconds
+//
+// golang does support leap seconds, so they must be added
+// Instead, golang uses a leap smear, which is
+// how Google production servers handle leap seconds,
+// smearing the additional second evenly across 24hrs
+// https://developers.google.com/time/smear
+func JulianDate(t time.Time) float64 {
+	unixWithLeap := t.Unix() + NumLeapSeconds(t) + TerrestrialTimeOffset
+	return float64(unixWithLeap) / SecondsPerDay + EpochJulianDate
+}
