26b6ab7ab78155f2acae47dc115bfd61822a5afe
Connor Demille <subtlepseudonym@gmail.com>
2020-06-30 12:41:30 -0400

    Use time.Time values rather than int64
    time.Time values are much more transparent and there aren't all that
    many of them, so there isn't a significant performance penalty

--- solar/leap.go
+++ solar/leap.go
@@ -5,36 +5,49 @@ import (
 	"sort"
 )
 
-// leapSeconds is the list of seconds after
-// which a leap second was added
-var leapSeconds = []int64{
-	78796799,
-	94694399,
-	126230399,
-	157766399,
-	189302399,
-	220924799,
-	252460799,
-	283996799,
-	315532799,
-	362793599,
-	394329599,
-	425865599,
-	489023999,
-	567993599,
-	631151999,
-	662687999,
-	709948799,
-	741484799,
-	773020799,
-	820454399,
-	867715199,
-	915148799,
-	1136073599,
-	1230767999,
-	1341100799,
-	1435708799,
-	1483228799,
+// leapSeconds is the list of time values to the nanosecond
+// after which a leap second was added
+//
+// Values are taken from this page on 29 June, 2020:
+// https://www.ietf.org/timezones/data/leap-seconds.list
+// 
+// The number of leap seconds to add to the calculated
+// Julian date can be determined by how many of these
+// values are lower than the unix time being converted.
+var leapSeconds = []time.Time{
+	getLeapSecondTime(1972, time.June, 30),
+	getLeapSecondTime(1972, time.December, 31),
+	getLeapSecondTime(1973, time.December, 31),
+	getLeapSecondTime(1974, time.December, 31),
+	getLeapSecondTime(1975, time.December, 31),
+	getLeapSecondTime(1976, time.December, 31),
+	getLeapSecondTime(1977, time.December, 31),
+	getLeapSecondTime(1978, time.December, 31),
+	getLeapSecondTime(1979, time.December, 31),
+	getLeapSecondTime(1981, time.June, 30),
+	getLeapSecondTime(1982, time.June, 30),
+	getLeapSecondTime(1983, time.June, 30),
+	getLeapSecondTime(1985, time.June, 30),
+	getLeapSecondTime(1987, time.December, 31),
+	getLeapSecondTime(1989, time.December, 31),
+	getLeapSecondTime(1990, time.December, 31),
+	getLeapSecondTime(1992, time.June, 30),
+	getLeapSecondTime(1993, time.June, 30),
+	getLeapSecondTime(1994, time.June, 30),
+	getLeapSecondTime(1995, time.December, 31),
+	getLeapSecondTime(1997, time.June, 30),
+	getLeapSecondTime(1998, time.December, 31),
+	getLeapSecondTime(2005, time.December, 31),
+	getLeapSecondTime(2008, time.December, 31),
+	getLeapSecondTime(2012, time.June, 30),
+	getLeapSecondTime(2015, time.June, 30),
+	getLeapSecondTime(2016, time.December, 31),
+}
+
+// getLeapSecondTime is shorthand for the time.Time representing the
+// final nanosecond before a leap second is added.
+func getLeapSecondTime(year int, month time.Month, day int) time.Time {
+	return time.Date(year, month, day, 23, 59, 59, 999999999, time.UTC)
 }
 
 // NumLeapSeconds calculates the number of leap seconds
@@ -46,61 +59,10 @@ var leapSeconds = []int64{
 //
 // Demonstration of the time package missing leap seconds:
 // https://play.golang.org/p/EcH0-dzh9w1
-func NumLeapSeconds(t time.Time) int {
-	u := t.Unix()
+func NumLeapSeconds(t time.Time) int64 {
 	idx := sort.Search(len(leapSeconds), func(i int) bool {
-		return u > leapSeconds[i]
+		return t.Before(leapSeconds[i])
 	})
 
-	return idx + 1
-}
-
-// calculateLeapSeconds is included purely to indicate
-// the method by which the "magic" integer values in
-// leapSeconds were obtained. Each value is
-// the unix time (in seconds) after which a leap second
-// was added.
-//
-// Values are taken from this page on 29 June, 2020:
-// https://www.ietf.org/timezones/data/leap-seconds.list
-// 
-// The number of leap seconds to add to the calculated
-// Julian date can be determined by how many of these
-// values are lower than the unix time being converted.
-func calculateLeapSeconds() []int64 {
-	return []int64{
-		getLeapSecondTime(1972, time.June, 30).Unix(),
-		getLeapSecondTime(1972, time.December, 31).Unix(),
-		getLeapSecondTime(1973, time.December, 31).Unix(),
-		getLeapSecondTime(1974, time.December, 31).Unix(),
-		getLeapSecondTime(1975, time.December, 31).Unix(),
-		getLeapSecondTime(1976, time.December, 31).Unix(),
-		getLeapSecondTime(1977, time.December, 31).Unix(),
-		getLeapSecondTime(1978, time.December, 31).Unix(),
-		getLeapSecondTime(1979, time.December, 31).Unix(),
-		getLeapSecondTime(1981, time.June, 30).Unix(),
-		getLeapSecondTime(1982, time.June, 30).Unix(),
-		getLeapSecondTime(1983, time.June, 30).Unix(),
-		getLeapSecondTime(1985, time.June, 30).Unix(),
-		getLeapSecondTime(1987, time.December, 31).Unix(),
-		getLeapSecondTime(1989, time.December, 31).Unix(),
-		getLeapSecondTime(1990, time.December, 31).Unix(),
-		getLeapSecondTime(1992, time.June, 30).Unix(),
-		getLeapSecondTime(1993, time.June, 30).Unix(),
-		getLeapSecondTime(1994, time.June, 30).Unix(),
-		getLeapSecondTime(1995, time.December, 31).Unix(),
-		getLeapSecondTime(1997, time.June, 30).Unix(),
-		getLeapSecondTime(1998, time.December, 31).Unix(),
-		getLeapSecondTime(2005, time.December, 31).Unix(),
-		getLeapSecondTime(2008, time.December, 31).Unix(),
-		getLeapSecondTime(2012, time.June, 30).Unix(),
-		getLeapSecondTime(2015, time.June, 30).Unix(),
-		getLeapSecondTime(2016, time.December, 31).Unix(),
-	}
-}
-
-// getLeapSecondTime is shorthand for the time.Time representing the
-// final nanosecond before a leap second is added.
-func getLeapSecondTime(year int, month time.Month, day int) time.Time {
-	return time.Date(year, month, day, 23, 59, 59, 999999999, time.UTC)
+	return int64(idx + 1)
 }
