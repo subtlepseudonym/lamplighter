3d3bc321cf1d5dcf4c6887611b6de4b4bddd4673
Connor Demille <subtlepseudonym@gmail.com>
2021-02-27 15:32:23 -0500

    Format solar/

--- solar/ecliptic.go
+++ solar/ecliptic.go
@@ -15,20 +15,20 @@ func MeanSolarNoon(julianDay, longitudeWest float64) float64 {
 // SolarMeanAnomaly calculates the fraction of the sun's
 // orbital period elapsed since perihelion.
 func SolarMeanAnomaly(meanSolarNoon float64) float64 {
-	return math.Mod(357.5291 + (0.98560028 * meanSolarNoon), 360)
+	return math.Mod(357.5291+(0.98560028*meanSolarNoon), 360)
 }
 
 // EquationOfTheCenter calculates the angular difference
 // between the position of the actual sun (with an elliptical
 // orbit) and the mean sun (with a circular orbit). This
-// can be expressed as a function of mean anomaly and 
+// can be expressed as a function of mean anomaly and
 // orbital eccentricity.
 //
 // https://en.wikipedia.org/wiki/Equation_of_the_center
 func EquationOfTheCenter(meanAnomaly float64) float64 {
 	firstOrder := 1.9148 * math.Sin(meanAnomaly)
-	secondOrder := 0.02 * math.Sin(2 * meanAnomaly)
-	thirdOrder := 0.0003 * math.Sin(3 * meanAnomaly)
+	secondOrder := 0.02 * math.Sin(2*meanAnomaly)
+	thirdOrder := 0.0003 * math.Sin(3*meanAnomaly)
 
 	return firstOrder + secondOrder + thirdOrder
 }
